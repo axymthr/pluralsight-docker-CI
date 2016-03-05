d9be88ee96fe64c1a05588a35870e4f201c1e2fe
awkshwayrd <mathur.aksh@gmail.com>
2016-03-06 00:00:01 +0530

    Fixed build error

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // Sample node.js web app for Pluralsight Docker CI course
 // For demonstration purposes only
-// added ssh keys
 'use strict';
 
 var express = require('express'),
     app = express();
 
-app.set('views', '/views');
+app.set('views', 'views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
